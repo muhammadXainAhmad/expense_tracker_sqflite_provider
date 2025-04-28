913001fe3499e2267e829003f6bd33471fc831e5
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-04-29 02:17:15 +0500

    Getting dependencies - created views folder

---
+++ lib/Views/category_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+  static const name = "category_screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Category Screen")));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_sqflite_provider/Views/category_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      initialRoute: CategoryScreen.name,
+      routes: {CategoryScreen.name: (context) => const CategoryScreen()},
     );
   }
 }
