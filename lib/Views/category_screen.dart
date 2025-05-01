05d3b0c210d075a3fa7c0d8c87a99d5911d5c940
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-05-01 19:14:10 +0500

    basic modalBtmSheet with dropDownBtn

--- lib/Views/category_screen.dart
+++ lib/Views/category_screen.dart
@@ -12,6 +12,88 @@ class CategoryScreen extends StatelessWidget {
         title: Column(children: [Text(r"$32,500.00"), Text("Total Balance")]),
         actions: [Icon(Icons.notifications_none_rounded, size: 36)],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 150,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton(
+                              dropdownColor: Colors.purple,
+                              items: const [
+                                DropdownMenuItem(
+                                  value: 'Cash',
+                                  child: Text('Cash'),
+                                ),
+                                DropdownMenuItem(
+                                  value: 'Card',
+                                  child: Text('Card'),
+                                ),
+                              ],
+                              onChanged: (value) {},
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 150,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton(
+                              items: const [
+                                DropdownMenuItem(
+                                  value: 'Transportation',
+                                  child: Text('Transportation'),
+                                ),
+                                DropdownMenuItem(
+                                  value: 'Sports',
+                                  child: Text('Sports'),
+                                ),
+                                DropdownMenuItem(
+                                  value: 'Food',
+                                  child: Text('Food'),
+                                ),
+                                DropdownMenuItem(
+                                  value: 'Entertainment',
+                                  child: Text('Entertainment'),
+                                ),
+                                DropdownMenuItem(
+                                  value: 'Education',
+                                  child: Text('Education'),
+                                ),
+                              ],
+                              onChanged: (value) {},
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
       drawer: Container(),
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: true,
@@ -76,8 +158,9 @@ class CategoryScreen extends StatelessWidget {
                     ),
                     subtitle: Text("Cash | Card"),
                     leading: Stack(
+                      alignment: Alignment.center,
                       children: [
-                        Icon(Icons.circle, color: Colors.purple),
+                        Icon(Icons.circle, color: Colors.purple, size: 40),
                         Icon(
                           Icons.airplanemode_active_rounded,
                           color: Colors.white,
