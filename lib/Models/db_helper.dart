9ccddccf37123d287b9fe806ab24072c9a890b10
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-05-03 00:34:28 +0500

    Added constants for column names in DBHelper

--- lib/Models/db_helper.dart
+++ lib/Models/db_helper.dart
@@ -1,14 +1,24 @@
 import 'dart:io';
 
 import 'package:expense_tracker_sqflite_provider/Constants/icons.dart';
-import 'package:expense_tracker_sqflite_provider/Models/expense_category.dart';
+import 'package:expense_tracker_sqflite_provider/Models/expense_category_model.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBHelper {
-  static final String cTable = "categoryTable";
-  static final String eTable = "expenseTable";
+  static final String cTableName = "categoryTable";
+  static final String eTableName = "expenseTable";
+  // cTable column names
+  static final String cTitle = "title";
+  static final String cTotalAmount = "totalAmount";
+
+  // eTable column names
+  static final String eId = "id";
+  static final String eTitle = "title";
+  static final String eAmount = "amount";
+  static final String eDate = "date";
+  static final String eCategory = "category";
 
   Database? _myDB;
 
@@ -25,32 +35,28 @@ class DBHelper {
       dbPath,
       onCreate: (db, version) async {
         await db.execute(
-          "create table $cTable (title TEXT PRIMARY KEY, entries INTEGER, totalAmount REAL)",
+          "create table $cTableName ($cTitle TEXT PRIMARY KEY, $cTotalAmount REAL)",
         );
         await db.execute(
-          "create table $eTable (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT, amount REAL, date TEXT, category TEXT)",
+          "create table $eTableName ($eId INTEGER PRIMARY KEY AUTOINCREMENT,$eTitle TEXT, $eAmount REAL, $eDate TEXT, $eCategory TEXT)",
         );
         for (var title in icons.keys) {
-          await db.insert(cTable, {
-            "title": title,
-            "entries": 0,
-            "totalAmount": 0.0,
-          });
+          await db.insert(cTableName, {"title": title, "totalAmount": 0.0});
         }
       },
       version: 1,
     );
   }
 
-  Future<List<ExpenseCategory>>getCategory() async {
+  Future<List<ExpenseCategory>> getCategory() async {
     var db = await getDB();
-    List<Map<String, dynamic>> mData = await db.query(cTable);
+    List<Map<String, dynamic>> mData = await db.query(cTableName);
     return mData.map((map) => ExpenseCategory.fromMap(map)).toList();
   }
 
   Future<List<Map<String, dynamic>>> getExpense() async {
     var db = await getDB();
-    List<Map<String, dynamic>> mData = await db.query(eTable);
+    List<Map<String, dynamic>> mData = await db.query(eTableName);
     return mData;
   }
 }
