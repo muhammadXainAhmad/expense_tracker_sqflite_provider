8b722b80a13aff29718398c21684f6a21b546575
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-05-05 00:51:49 +0500

    Fetching data in ExpenseScreen

--- lib/Views/expense_screen.dart
+++ lib/Views/expense_screen.dart
@@ -1,4 +1,6 @@
+import 'package:expense_tracker_sqflite_provider/Providers/add_expense_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ExpenseScreen extends StatelessWidget {
   const ExpenseScreen({super.key});
@@ -6,9 +8,34 @@ class ExpenseScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<AddExpenseProvider>().getExpense();
+    });
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Expense Screen")
+      appBar: AppBar(title: Text("Expense Screen")),
+      body: Center(
+        child: Column(
+          children: [
+            Consumer<AddExpenseProvider>(
+              builder: (context, provider, child) {
+                return provider.expenseList.isEmpty
+                    ? Center(child: Text("No expenses found!"))
+                    : Expanded(
+                      child: ListView.builder(itemCount: provider.expenseList.length,
+                        itemBuilder: (context, index) {
+                          final expense = provider.expenseList[index];
+                          return ListTile(
+                            title: Text(expense.title),
+                            subtitle: Text("\$${expense.amount}"),
+                            trailing: Text(expense.date.toString().split(" ")[0]),
+                          );
+                        },
+                      ),
+                    );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
