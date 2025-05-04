87b5b9b9b2bcc93f05b3da1ce28844e0b035cf6c
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-05-04 23:05:39 +0500

    Implemented datePicker with provider

--- lib/Providers/add_expense_provider.dart
+++ lib/Providers/add_expense_provider.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 class AddExpenseProvider with ChangeNotifier {
   bool _showPrefix = false;
   bool get showPrefix => _showPrefix;
-  void updatePrefixText(String text){
-    _showPrefix=text.isNotEmpty;
+  void updatePrefixText(String text) {
+    _showPrefix = text.isNotEmpty;
     notifyListeners();
   }
+
+  DateTime _selectedDate = DateTime.now();
+  DateTime get selectedDate => _selectedDate;
+
+  void updateSelectedDate(DateTime date) {
+    _selectedDate = date;
+    notifyListeners();
+  }
+
+  void addExpenseItem() async {}
 }
