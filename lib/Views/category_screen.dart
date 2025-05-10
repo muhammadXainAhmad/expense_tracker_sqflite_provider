1b7ff3e78a09716ef3426fa783787616df68832a
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-05-11 00:14:30 +0500

    Added income/expense card - fixed bug: expenses not displaying within categories

--- lib/Views/category_screen.dart
+++ lib/Views/category_screen.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker_sqflite_provider/Constants/icons.dart';
 import 'package:expense_tracker_sqflite_provider/Providers/category_provider.dart';
+import 'package:expense_tracker_sqflite_provider/Providers/expense_provider.dart';
 import 'package:expense_tracker_sqflite_provider/Views/category_expense_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -12,6 +13,8 @@ class CategoryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       Provider.of<CategoryProvider>(context, listen: false).fetchCategories();
+      Provider.of<ExpenseProvider>(context, listen: false).getExpense();
+    
     });
     return Scaffold(
       appBar: AppBar(
@@ -20,107 +23,133 @@ class CategoryScreen extends StatelessWidget {
         actions: [Icon(Icons.notifications_none_rounded, size: 30)],
       ),
 
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                      borderRadius: BorderRadius.circular(20),
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 200,
+            child: Card(
+              margin: EdgeInsets.all(32),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              elevation: 2,
+              color: Colors.tealAccent.shade400,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Total Balance",
+                      style: TextStyle(color: Colors.black, fontSize: 16),
                     ),
-                    child: Center(child: Text("Day")),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                      borderRadius: BorderRadius.circular(20),
+                    Text(
+                      r"$ 32,500",
+                      style: TextStyle(color: Colors.black, fontSize: 38),
                     ),
-                    child: Center(child: Text("Week")),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Container(
-                      width: 100,
-                      height: 80,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade200,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Center(child: Text("Month")),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [Text("Income"), Text("Expenses")],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            Consumer<CategoryProvider>(
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 100,
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(child: Text("Day")),
+              ),
+              Container(
+                width: 100,
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(child: Text("Week")),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  width: 100,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(child: Text("Month")),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Consumer<CategoryProvider>(
               builder: (context, categoryProvider, child) {
                 if (categoryProvider.isLoading) {
                   return Center(child: CircularProgressIndicator());
                 }
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: categoryProvider.categories.length,
-                    itemBuilder: (context, index) {
-                      final category = categoryProvider.categories[index];
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          onTap: () {
-                            Navigator.pushNamed(
-                              context,
-                              CategoryExpenseScreen.name,
-                              arguments: category.title,
-                            );
-                          },
-                          leading: Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              Icon(
-                                Icons.circle,
-                                size: 50,
-                                color:
-                                    categoryColors[category.title] ??
-                                    Colors.grey,
-                              ),
-                              Icon(
-                                icons[category.title] ?? Icons.category,
-                                size: 26,
-                                color: Colors.black,
-                              ),
-                            ],
-                          ),
-                          title: Text(
-                            category.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
+                return ListView.builder(
+                  itemCount: categoryProvider.categories.length,
+                  itemBuilder: (context, index) {
+                    final category = categoryProvider.categories[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            CategoryExpenseScreen.name,
+                            arguments: category.title,
+                          );
+                        },
+                        leading: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Icon(
+                              Icons.circle,
+                              size: 50,
+                              color:
+                                  categoryColors[category.title] ??
+                                  Colors.grey,
                             ),
-                          ),
-                          trailing: Text(
-                            '\$${category.totalAmount}',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+                            Icon(
+                              icons[category.title] ?? Icons.category,
+                              size: 26,
+                              color: Colors.black,
                             ),
+                          ],
+                        ),
+                        title: Text(
+                          category.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
                           ),
                         ),
-                      );
-                    },
-                  ),
+                        trailing: Text(
+                          '\$${category.totalAmount}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 );
               },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
